118d610bd39077f916b6111b9d717ed87b567a4d
CrimsonTLU <chris55@tlu.ee>
2023-03-09 22:51:26 +0200

    initial setup (carousel)

---
+++ mytv-proov/pages/index.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div>
+    <VueSlickCarousel :arrows="true" :dots="false">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </VueSlickCarousel>
+  </div>
+</template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+
+export default {
+  name: 'IndexPage',
+  components: { VueSlickCarousel },
+}
+</script>
