1eb6aae854b68dd11f2351074d2187e4c7779826
CrimsonTLU <chris55@tlu.ee>
2023-04-12 21:35:19 +0300

    fixes

--- mytv-proov/pages/index.vue
+++ mytv-proov/pages/index.vue
@@ -1,44 +1,58 @@
-
-<template>
-  <div>
-    <VueSlickCarousel :arrows="true" :dots="false">
-      <!-- <img v-for="(item, index) in photo">
-      "https://source.unsplash.com/"{{ photo.id }} -->
-
-      <img src="https://source.unsplash.com/random/" alt="2">
-      <img src="https://source.unsplash.com/random/" alt="3">
-      <img src="https://source.unsplash.com/random/" alt="4">
-      <img src="https://source.unsplash.com/random/" alt="5">
-    </VueSlickCarousel>
-  </div>
-</template>
-
 <script>
-
 import { createApi } from 'unsplash-js';
-import VueSlickCarousel from 'vue-slick-carousel'
-import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import VueSlickCarousel from 'vue-slick-carousel';
+import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+import VueFullscreen from 'vue-fullscreen';
+import Vue from 'vue';
+import { ref, computed } from 'vue';
+
+Vue.use(VueFullscreen);
 
 const unsplash = createApi({ accessKey: process.env.api_key });
 
-unsplash.photos.getRandom({
-  count: 5,
-}).then(result => {
-  if (result.errors) {
-
-    console.log('error occurred: ', result.errors[0]);
-  } else {
-
-    const photo = result.response;
-    console.log(photo[0].id);
-    return {
-      photo: photo
-    }
-  }
-});
-
 export default {
   name: 'IndexPage',
-  components: { VueSlickCarousel },
+  components: {
+    VueSlickCarousel
+  },
+  data() {
+    return {
+      photos: []
+    }
+  },
+  computed: {
+    imageUrl() {
+      return (id) => {
+        return "https://source.unsplash.com/" + id;
+      }
+    }
+  },
+  mounted() {
+    unsplash.photos.getRandom({
+      count: 5,
+    }).then(result => {
+      if (result.errors) {
+        console.log('error occurred: ', result.errors[0]);
+      } else {
+        this.photos = result.response;
+        console.log(this.photos[3].id);
+      }
+    });
+  }
 }
 </script>
+
+<template>
+  <div>
+    <template v-if="photos.length">
+      <VueSlickCarousel id="imagecarousel" :arrows="true" :dots="false" :autoplay="true" :adaptiveHeight="true">
+        <div v-for="photo in photos" :key="photo.id" id="image-wrapper" class="w-screen h-screen overflow-x-hidden overflow-y-auto">
+          <img :src="imageUrl(photo.id)">
+        </div>
+        
+      </VueSlickCarousel>
+    </template>
+    <img v-else>
+  </div>
+</template>
